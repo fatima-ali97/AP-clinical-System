b519234aca526976f21843d23c9e094ff357e317
Fatima <202304056@student.polytechnic.bh>
2026-03-29 23:51:31 +0300

    setup

---
+++ AP-clinical-system/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AP_clinical_system.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
