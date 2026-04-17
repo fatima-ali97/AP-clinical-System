c34285d1d3ae2785bdb49bc960044fc81dae1d43
Fatima <202304056@student.polytechnic.bh>
2026-04-17 11:49:39 +0300

    add doctor main views

---
+++ AP-clinical-system/Controllers/DoctorController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AP_clinical_system.Controllers
+{
+    public class DoctorController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET
+        public ActionResult Schedule()
+        {
+            return View();
+        }
+        // GET
+        public ActionResult Patients()
+        {
+            return View();
+        }
+
+
+        public ActionResult Prescriptions()
+        {
+            return View();
+        }
+
+
+
+    }
+}
---
+++ AP-clinical-system/Controllers/ReceptionestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AP_clinical_system.Controllers
+{
+    public class ReceptionistController : Controller
+    {
+
+        // GET: PatientController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET
+        public ActionResult Book()
+        {
+            return View();
+        }
+        // GET
+        public ActionResult Notifications()
+        {
+            return View();
+        }
+
+
+
+    }
+}
