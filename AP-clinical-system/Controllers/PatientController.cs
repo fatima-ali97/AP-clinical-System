64e48bde04507281ffea4283357149b38ee97e76
Fatima <202304056@student.polytechnic.bh>
2026-04-17 11:25:59 +0300

    fix icons, add patient main views

--- AP-clinical-system/Controllers/PatientController.cs
+++ AP-clinical-system/Controllers/PatientController.cs
@@ -5,80 +5,33 @@ namespace AP_clinical_system.Controllers
 {
     public class PatientController : Controller
     {
-        // GET: HomeController1
-
+        // GET: PatientController
         public ActionResult Index()
         {
             return View();
         }
 
-        // GET: HomeController1/Details/5
-        public ActionResult Details(int id)
+        // GET
+        public ActionResult Appointments()
+        {
+            return View();
+        }
+        // GET
+        public ActionResult Prescriptions()
         {
             return View();
         }
 
-        // GET: HomeController1/Create
-        public ActionResult Create()
+
+        public ActionResult Notifications()
         {
             return View();
         }
 
-        // POST: HomeController1/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomeController1/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult History()
         {
             return View();
         }
 
-        // POST: HomeController1/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: HomeController1/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: HomeController1/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
--- AP-clinical-system/services/NavigationService.cs
+++ AP-clinical-system/services/NavigationService.cs
@@ -7,36 +7,35 @@
         {
             "Manager" => new List<NavItem>
         {
-            new() { Label = "Dashboard",    Controller = "Manager",      Action = "Index",         Icon = "bi-grid" },
-            new() { Label = "Doctors",      Controller = "Manager",      Action = "Doctors",       Icon = "bi-person-badge" },
-            new() { Label = "Appointments", Controller = "Manager",      Action = "Appointments",  Icon = "bi-calendar" },
-            new() { Label = "Reports",      Controller = "Manager",      Action = "Reports",       Icon = "bi-bar-chart" },
-            new() { Label = "Users",        Controller = "Manager",      Action = "Users",         Icon = "bi-people" },
+            new() { Label = "Dashboard",    Controller = "Manager",      Action = "Index",         Icon = "fa-solid fa-table-cells-large" },
+            new() { Label = "Doctors",      Controller = "Manager",      Action = "Doctors",       Icon = "fa-solid fa-user-doctor" },
+            new() { Label = "Appointments", Controller = "Manager",      Action = "Appointments",  Icon ="fa-solid fa-calendar" },
+            new() { Label = "Reports",      Controller = "Manager",      Action = "Reports",       Icon = "fa-solid fa-chart-line" },
+            new() { Label = "Users",        Controller = "Manager",      Action = "Users",         Icon = "fa-solid fa-user-gear" },
         },
 
             "Doctor" => new List<NavItem>
         {
-            new() { Label = "Dashboard",    Controller = "Doctor",       Action = "Index",         Icon = "bi-grid" },
-            new() { Label = "My Schedule",  Controller = "Doctor",       Action = "Schedule",      Icon = "bi-calendar-check" },
-            new() { Label = "My Patients",  Controller = "Doctor",       Action = "Patients",      Icon = "bi-person-heart" },
-            new() { Label = "Prescriptions",Controller = "Doctor",       Action = "Prescriptions", Icon = "bi-capsule" },
+            new() { Label = "Dashboard",    Controller = "Doctor",       Action = "Index",         Icon = "fa-solid fa-table-cells-large" },
+            new() { Label = "My Schedule",  Controller = "Doctor",       Action = "Schedule",      Icon = "fa-solid fa-calendar"},
+            new() { Label = "My Patients",  Controller = "Doctor",       Action = "Patients",      Icon = "fa-solid fa-bed-pulse" },
+            new() { Label = "Prescriptions",Controller = "Doctor",       Action = "Prescriptions", Icon = "fa-solid fa-capsules" },
         },
 
             "Receptionist" => new List<NavItem>
         {
-            new() { Label = "Dashboard",    Controller = "Receptionist", Action = "Index",         Icon = "bi-grid" },
-            new() { Label = "Book Appointment", Controller = "Receptionist", Action = "Book",      Icon = "bi-calendar-plus" },
-            new() { Label = "Live Queue",   Controller = "Receptionist", Action = "Queue",         Icon = "bi-display" },
-            new() { Label = "Notifications",     Controller = "Receptionist", Action = "Patients",      Icon = "bi-people" },
+            new() { Label = "Dashboard",    Controller = "Receptionist", Action = "Index",         Icon = "fa-solid fa-table-cells-large" },
+            new() { Label = "Book Appointment", Controller = "Receptionist", Action = "Book",      Icon = "fa-solid fa-plus" },
+            new() { Label = "Notifications",     Controller = "Receptionist", Action = "Notifications",      Icon = "fa-solid fa-bell"  },
         },
 
             "Patient" => new List<NavItem>
         {
-            new() { Label = "Dashboard",    Controller = "Patient",      Action = "Index",         Icon = "bi-grid" },
-            new() { Label = "My Appointments", Controller = "Patient",   Action = "Appointments",  Icon = "bi-calendar" },
-            new() { Label = "Prescriptions",Controller = "Patient",      Action = "Prescriptions", Icon = "bi-capsule" },
-            new() { Label = "Notifications",Controller = "Patient",      Action = "Prescriptions", Icon = "bi-capsule" },
-            new() { Label = "History",   Controller = "Patient",      Action = "Records",       Icon = "bi-file-medical" },
+            new() { Label = "Dashboard",    Controller = "Patient",      Action = "Index",         Icon = "fa-solid fa-table-cells-large" },
+            new() { Label = "My Appointments", Controller = "Patient",   Action = "Appointments",  Icon = "fa-solid fa-calendar" },
+            new() { Label = "Prescriptions",Controller = "Patient",      Action = "Prescriptions", Icon = "fa-solid fa-capsules" },
+            new() { Label = "Notifications",Controller = "Patient",      Action = "Notifications", Icon = "fa-solid fa-bell" },
+            new() { Label = "History",   Controller = "Patient",      Action = "History",       Icon = "fa-solid fa-clock-rotate-left" },
         },
 
             _ => new List<NavItem>()
