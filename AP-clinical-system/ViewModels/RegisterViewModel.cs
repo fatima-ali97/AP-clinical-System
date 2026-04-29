87306d1aa7e660547f9e74843e1aed817a557c0b
HoorHasan30 <202301820@student.polytechnic.bh>
2026-04-30 00:18:54 +0300

    updates

---
+++ AP-clinical-system/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AP_clinical_system.ViewModels
+{
+    public class RegisterViewModel
+    {
+        //First Name
+        [Required]
+        [Display(Name = "First Name")]
+        public required string First_Name { get; set; }
+
+        //Last Name
+        [Required]
+        [Display(Name = "Last Name")]
+        public required string Last_Name { get; set; }
+
+        //CPR
+        [Required]
+        [Display(Name = "CPR")]
+        public required string CPR { get; set; }
+
+        //Phone Number 
+        [Required]
+        [Display(Name = "Phone Number")]
+        public required string Phone_Number { get; set; }
+
+        //Email
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public required string Email { get; set; }
+
+        //Password
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 8,
+            ErrorMessage = "Password must be at least {2} characters long.")]
+        [Display(Name = "Password")]
+        public required string Password { get; set; }
+
+        //Confirm Password
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password",
+            ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Confirm Password")]
+        public required string ConfirmPassword { get; set; }
+
+    }
+}
