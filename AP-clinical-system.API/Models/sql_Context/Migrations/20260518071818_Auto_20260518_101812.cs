590097a34b18894dd6131dd41a99c6affdf2397d
44Aljamri <abbasfadhel499@gmail.com>
2026-05-18 10:19:09 +0300

    Added medical conditions field to patient

---
+++ AP-clinical-system.API/Models/sql_Context/Migrations/20260518071818_Auto_20260518_101812.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AP_clinical_system.Models.sql_Context.Migrations
+{
+    /// <inheritdoc />
+    public partial class Auto_20260518_101812 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "medical_conditions",
+                table: "patient_informations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "medical_conditions",
+                table: "patient_informations");
+        }
+    }
+}
